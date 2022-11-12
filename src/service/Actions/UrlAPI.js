9ce155d6cbb6a42556f06b930bce895a46bdbe14
Sơn Nguyễn <sonnguyencreater@gmail.com>
2022-11-12 17:47:03 +0700

    fix
    update

--- src/service/Actions/UrlAPI.js
+++ src/service/Actions/UrlAPI.js
@@ -3,14 +3,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8080"
-    : // "http://localhost:3000"
-      "https://sleepy-inlet-56101.herokuapp.com/api";
+    : "https://sleepy-inlet-56101.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "4NutritionHealthy";
-
-// export const POSTS_LOADED_SUCCESS = "POSTS_LOADED_SUCCESS";
-// export const POSTS_LOADED_FAIL = "POSTS_LOADED_FAIL";
-// export const ADD_POST = "ADD_POST";
-// export const DELETE_POST = "DELETE_POST";
-// export const UPDATE_POST = "UPDATE_POST";
-// export const FIND_POST = "FIND_POST";
