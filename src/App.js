f13e32a8d82fa77493f700124e739d908bda971e
longnvhe140051@fpt.edu.vn <longnvhe140051@fpt.edu.vn>
2022-11-28 15:26:37 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./assets/style/common/App.css";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
-import AuthContextProvider from "./service/Actions/UserAPI";
+import AuthContextProvider from "./service/Actions/AuthAPI";
 import "antd/dist/antd.css";
 import "./assets/style/common/layout.css";
 import Routes from "./routes/Routes";
 import "./assets/style/admin/style.css";
+
 function App() {
   return (
     <AuthContextProvider>
