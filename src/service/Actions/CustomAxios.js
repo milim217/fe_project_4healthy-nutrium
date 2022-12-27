24a7a194087476c242ddaa6d954c38cd81c433e3
milim217 <sonndhe140443@fpt.edu.vn>
2022-12-28 00:58:11 +0700

    update authorization

---
+++ src/service/Actions/CustomAxios.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8080",
+  headers: {
+      'Content-Type': 'application/json',
+  }
+});
+
+axiosInstance.interceptors.request.use(async (req) => {
+  const token = localStorage.getItem("jwt")
+  req.headers.Authorization = token
+  return req
+}
+)
+export default axiosInstance
+
+
